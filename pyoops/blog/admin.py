b73f3b1f85e30afc2435849b0b634cd1361fd233
pseudolukiian <pseudokote@gmail.com>
2022-12-07 12:20:02 +0000

    Alpha_0.4

--- pyoops/blog/admin.py
+++ pyoops/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import blog_post, blog_tag, blog_comment
+from .models import blog_post, blog_tag, blog_comment, blog_post_category, blog_post_level
 
 admin.site.register(blog_post)
 admin.site.register(blog_tag)
 admin.site.register(blog_comment)
-
+admin.site.register(blog_post_category)
+admin.site.register(blog_post_level)

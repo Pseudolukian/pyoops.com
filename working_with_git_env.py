0364953d46f3501721cf5512af6822d68ac11f45
pseudolukiian <pseudokote@gmail.com>
2023-01-15 20:41:22 +0000

    Fix test env python scr

--- working_with_git_env.py
+++ working_with_git_env.py
@@ -1,17 +1,5 @@
 import os
 
-env_file = os.getenv('GITHUB_ENV')
-server_ip = "123.213.341.21"
-print(1)
-print(2)
-print(env_file)
-
-
-with open(env_file, "a") as file: 
-    file.write('VPS=asd')
-
-print(1)
-print(2)
-print(env_file)
+os.system('echo VPS=123 >> $GITHUB_ENV')
 
 
